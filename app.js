c08ea2411def1e10b6e10f6dbcfd82e751e2682d
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-04-19 16:05:06 -0500

    second

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-$(document).ready(function ({
+$(document).ready(function (){
+console.log('Yay');
 
+$('#addEmployee').on('submit', function (event) {
+    event.preventDefault();
 
+    let entry = {};
 
+    $(this).serializeArray().forEach(function (item) {
+        entry[item.name] = item.value;
+    });
 
+    $(this).trigger('reset');
 
-
-
-    
+    console.log(entry);
+    employeeArray.push(entry);
+    render();
 });
+
+
+
+
+});
+
+let employeeArray = [];
+
+function render(){
+    $('.container').empty();
+    for (let i = 0; i < employeeArray.length; i++){
+        $('.container').append('<div></div>');
+        let newDiv = $('.container').children().last();
+        let person = employeeArray[i];
+        newDiv.append('<p>' + person.firstName + '</p>');
+        newDiv.append('<p>' + person.lastName + '</p>');
+        newDiv.append('<p>' + person.employeeID + '</p>');
+        newDiv.append('<p>' + person.title + '</p>');
+        newDiv.append('<p>' + person.annualSalary + '</p>');
+
+    }
+}
